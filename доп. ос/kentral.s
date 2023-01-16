0ca4ab561a1bae6b9ac8732c0c3dc515245229c2
AkameWeb <116306258+DaniilScl@users.noreply.github.com>
2023-01-16 21:23:51 +0400

    OS

---
+++ доп. ос/kentral.s
@@ -0,0 +1,78 @@
+
+
+
+
+bits 32			;директива nasm - 32 bit
+section .text
+
+global start
+extern kmain	        ;kmain определена в C-файле
+
+start:
+  cli 			;блокировка прерываний
+  mov esp, stack_space	;установка указателя стека
+  call kmain
+  hlt		 	;остановка процессора
+  
+  
+  
+  
+  
+
+section .bss
+resb 8192		;8KB на стек
+stack_space:
+
+
+
+
+
+
+
+section .text
+     use16
+     org  0x7C00  ; наша программа загружается по адресу 0x7C00
+start:
+
+
+     mov  ax, cs
+     mov  ds, ax  ; выбираем сегмент данных
+ 
+     mov  si, message
+     cld              ; направление для строковых команд
+     mov  ah, 0x6E    ; номер функции BIOS
+     mov  bh, 0x10    ; страница видеопамяти
+puts_loop:
+
+
+
+
+     lodsb            ; загружаем очередной символ в al
+     test al, al      ; нулевой символ означает конец строки
+     jz   puts_loop_exit
+     int  0x10        ; вызываем функцию BIOS
+     jmp  puts_loop
+puts_loop_exit:
+     jmp  $           ; вечный цикл
+     
+     
+ 
+message:
+     db   'Hello World!', 0
+finish:
+     times 0x1FE-finish+start db 0
+     db   0x55, 0xAA  ; сигнатура загрузочного сектора
+     
+
+     
+
+     
+     
+   
+     
+     mov         ax, cs
+    mov         ds, ax
+    
+    
+ 
+    
